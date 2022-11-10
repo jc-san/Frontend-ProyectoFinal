7ed3bcbd2af96580f6a44d89a9824ae42ea473e4
Jesus Manuel Juarez Trejo <jesus.juarezt@coppel.com>
2022-11-09 21:42:56 -0700

    Actualización
    - Se agrega información del proyecto.
    - Se actualizan los archivos de estilos para los usuarios de github.
    - Se agregan usuarios de github cargados dinámicamente mediante componentes.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import ShowParticipantes from '../components/ShowParticipantes' 
+import ShowUsers from '../components/ShowUsersGithub' 
 import Participante from '../Participantes';
+import Sensei from '../Senseis';
+
 
 import { Typography } from '@mui/material';
 
 
 function AboutPage() {
     return (
-
         <>
             <Typography variant="h3" align='center' sx={{ pt: 10, pb:7}}>
                 About
             </Typography>
 
+            <Typography variant="h5" gutterBottom>
+                Proyecto Final Kata 5 - Frontend:
+            </Typography>
+            
+            <Typography variant="body1" gutterBottom sx={{pt: 2, pb:2}}>
+                El presente proyecto es un catálogo de películas de Marvel, donde se obtiene la información de cada película de la API api.themoviedb.org.            
+            </Typography>
+            <Typography variant="h6" gutterBottom sx={{pt: 3, pb:1}}>
+                Participantes del proyecto: 
+            </Typography>
+            <div className="wrapper-user" >
+                {Participante.map((item)=> (
+                    <ShowUsers nombre={item.name} usuario={item.user}/>
+                ))}
+            </div>
+            <Typography variant="h6" gutterBottom sx={{pt: 3, pb:1}}> 
+                Senseis: 
+            </Typography>
+            <div className="wrapper-user">
+                {Sensei.map((item)=> (
+                    <ShowUsers nombre={item.name} usuario={item.user}/>
+                ))}
+            </div>
 
-            {Participante.map((item)=> (
-                <ShowParticipantes nombre={item.name} usuario={item.user}  margin="normal"/>
-            ))
-            }
         </>
     );
 }
